ba2623bf4fe2fb6188885047cd080ab7493485aa
SosukeKato <dokindokinpiano@gmail.com>
2025-07-23 21:49:46 +0900

    [Update]_rbでRigidbody2Dを呼び出す処理を追加

--- Assets/_WorkSpace/Script/PlayerMove.cs
+++ Assets/_WorkSpace/Script/PlayerMove.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,5 +16,7 @@ public class PlayerMove : MonoBehaviour
     {
         //x²‚ÌˆÚ“®ˆ—
         float x = Input.GetAxisRaw("Horizontal");
+        //y²‚ÌˆÚ“®ˆ—
+
     }
 }
