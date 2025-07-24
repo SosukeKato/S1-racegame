6be6192bac8ace4c3dbe332d3cc050b45449f8b6
SosukeKato <dokindokinpiano@gmail.com>
2025-07-24 19:46:43 +0900

    [Update]スポーンさせるキャラクターに合わせてスポーンさせる処理を追加

--- Assets/_WorkSpace/Script/CharacterSpawn.cs
+++ Assets/_WorkSpace/Script/CharacterSpawn.cs
@@ -6,13 +6,14 @@ public class CharacterSpawn : MonoBehaviour
 {
     [SerializeField]
     List<GameObject> _SpawnCharacter;
-    [SerializeField]
-    List<Transform> _CharacterSpawnPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int CharacterSpawnNumber = 0; CharacterSpawnNumber < _SpawnCharacter.Count; CharacterSpawnNumber++)
+        {
+            Instantiate(_SpawnCharacter[CharacterSpawnNumber], new Vector3(CharacterSpawnNumber, 0, 0), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
