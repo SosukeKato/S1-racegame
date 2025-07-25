9c337e63cd499a81e16fa0e0dc861fea93529631
SosukeKato <dokindokinpiano@gmail.com>
2025-07-25 16:36:31 +0900

    [Update]Characterが_minXから_maxXの間でのみ移動できるようにする処理を追加

--- Assets/_WorkSpace/Script/CharacterClamp.cs
+++ Assets/_WorkSpace/Script/CharacterClamp.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CharacterClamp : MonoBehaviour
 {
+    [SerializeField]
+    float _minX = 0f;
+    [SerializeField]
+    float _maxX = 11f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class CharacterClamp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 pos = transform.position;
+
+        pos.x = Mathf.Clamp(pos.x, _minX, _maxX);
+
+        transform.position = pos;
     }
 }
