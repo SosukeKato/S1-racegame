0f8250cb5348fc5ebc81424b67c8d393676373da
SosukeKato <dokindokinpiano@gmail.com>
2025-07-26 20:04:35 +0900

    [Update]NPCのy軸の移動処理を追加

--- Assets/_WorkSpace/Script/EnemyAI.cs
+++ Assets/_WorkSpace/Script/EnemyAI.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    Rigidbody2D _rb;
+    [SerializeField]
+    int _DirectinalSpeed;
+
+    float _SpeedMagnification = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _rb.velocity = new Vector2(_rb.velocity.x, _DirectinalSpeed * _SpeedMagnification);
     }
 }
