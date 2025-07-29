ddc8ad29ed03f1810199162ff39712f0c4ea397c
SosukeKato <dokindokinpiano@gmail.com>
2025-07-29 18:04:20 +0900

    [Update]CameraがPlayerに追従する処理を追加

--- Assets/_WorkSpace/Script/Camera.cs
+++ Assets/_WorkSpace/Script/Camera.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    PlayerMove _FindPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _FindPlayer = FindAnyObjectByType<PlayerMove>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = _FindPlayer.transform.position.x;
+        float y = _FindPlayer.transform.position.y;
+        transform.position = new Vector3(x, y, transform.position.z);
     }
 }
