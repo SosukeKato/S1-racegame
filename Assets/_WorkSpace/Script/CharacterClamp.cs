af68bff869b922357d1349e1b2757effbcf4e98e
SosukeKato <dokindokinpiano@gmail.com>
2025-07-31 22:34:37 +0900

    [fix]CharacterClamp内の不要な処理を削除

--- Assets/_WorkSpace/Script/CharacterClamp.cs
+++ Assets/_WorkSpace/Script/CharacterClamp.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterClamp : MonoBehaviour
@@ -8,13 +6,6 @@ public class CharacterClamp : MonoBehaviour
     float _minX = 0f;
     [SerializeField]
     float _maxX = 11f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Vector2 pos = transform.position;
